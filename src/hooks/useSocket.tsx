52322f4a77810bcf16b939b7742fc89dd596eb51
moath alafifi <moath.fayez.alafifi@gmail.com>
2023-02-12 20:32:24 +0200

    socket

--- src/hooks/useSocket.tsx
+++ src/hooks/useSocket.tsx
@@ -1,26 +1,44 @@
-import { useEffect, useState } from 'react'
+import { AuthContext } from '@/contexts/AuthContext'
+import axios from 'axios'
+import { useContext, useEffect, useState } from 'react'
 import { io } from 'Socket.io-client'
 
 const useSocket = () => {
   const [socket, setSocket] = useState(null)
+  const [conversations, setConversations] = useState([])
+  const { user } = useContext(AuthContext)
+  
   useEffect(() => {
-    fetch('/api/socket').finally(() => {
+    const socketInitializer  = async () => {
+      await fetch('/api/socket')
       const socketIo = io()
-      socketIo.on('connect', () => {
-        console.log('connect')
-      })
-      socketIo.on('disconnect', () => {
-        console.log('disconnect')
-      })
       setSocket(socketIo)
-    })
+    }
+    socketInitializer ()
+
     const cleanup = () => {
       socket?.disconnect()
     }
     return cleanup
   }, [])
+ 
+  useEffect(() => {
+    socket?.emit('addUser', user?._id )
+    const getConversations = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:3000/api/conversation/${user?._id}`
+        )
+        setConversations(res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getConversations()
+  }, [user?._id])
 
-  return socket
+
+  return { socket, conversations }
 }
 
 export default useSocket
