cf41aaa5a0f14ef129b873b0a211a884c2f67827
moath alafifi <moath.fayez.alafifi@gmail.com>
2023-02-14 19:05:45 +0200

    api

--- src/pages/api/apiCalls/conversations.ts
+++ src/pages/api/apiCalls/conversations.ts
@@ -1,18 +1,16 @@
-import useGetUser from '@/hooks/useGetUser'
 import axios from 'axios'
- 
 
-export const getConversation = async () => {
-
+export const getConversation = async (userId: any) => {
   const { data: conversation } = await axios.get(
-    'http://localhost:3000/api/conversation'
+    'http://localhost:3000/api/conversation/' + userId
   )
   return conversation
 }
 
 export const addConversation = async (data) => {
   const { data: conversation } = await axios.post(
-    'http://localhost:3000/api/conversation',data
+    'http://localhost:3000/api/conversation',
+    data
   )
   return conversation
 }
--- src/pages/api/apiCalls/message.ts
+++ src/pages/api/apiCalls/message.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const addMessage = async (data) => {
   const { data: message } = await axios.post(
-    'http://localhost:3000/api/Message',data
+    'http://localhost:3000/api/message',data
   )
   return message
 }
